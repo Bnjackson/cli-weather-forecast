4087d343318d2319395a70801bd83b3b88b779a3
benjac <bnjackson951@gmail.com>
2023-10-05 11:54:43 +0100

    Add functions to get and verify user location input and user choice input

---
+++ utils/input.js
@@ -0,0 +1,31 @@
+const readLineSync = require('readline-sync');
+
+function getUserLocation() {
+    const userLocationInput = readLineSync.question('What is the location? ').toLowerCase();
+    return checkUserInput(userLocationInput) ? userLocationInput : getUserLocation();
+    function checkUserInput(userInput) {
+        if (userInput) {
+            return true;
+        } else {
+            console.log('You must enter a value when inputting a location');
+            return false;
+        }
+    }
+}
+
+function getUserChoice(question, correctAnswers) {
+    const userChoice = readLineSync.question(`${question}`).toLowerCase();
+    return checkUserChoice() ? userChoice : getUserChoice(question, correctAnswers);
+    function checkUserChoice() {
+        if (correctAnswers.includes(userChoice)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+module.exports = {
+    getUserLocation,
+    getUserChoice
+}
