6914c579294522f7ad856f39e1701a55d989d492
benjac <bnjackson951@gmail.com>
2023-10-06 17:26:17 +0100

    Make main function asynchronous, add space to the end of question variable string, changed name of getWeatherForecast.js so have to change import module name

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 'use strict';
 
 const inputModule = require('./utils/input.js');
+const getCurrentModule = require('./commands/getCurrent.js');
 
 console.log(
 `This program takes a location input from the command line and outputs weather data for that location.`
 );
 
-function main() {
+async function main() {
     let continueRunning = true;
     while (continueRunning) {
         const userLocation = inputModule.getUserLocation();
-        console.log(userLocation);
+        await getCurrentModule.outputWeatherData(userLocation);
         checkAnotherlocation() ? continueRunning = true : continueRunning = false;
     }
+    console.log('Thank you for using this program.');
 }
 
 function checkAnotherlocation() {
-    const question = 'Would you like to enter another location? ';
+    const question = 'Would you like to enter another location? yes/no ';
     const correctAnswers = ['yes', 'y', 'no', 'n'];
     const userChoice = inputModule.getUserChoice(question, correctAnswers);
     if (userChoice === 'yes' || userChoice === 'y') {
